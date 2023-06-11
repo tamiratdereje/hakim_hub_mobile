e9ae2accce96717a2c0f3b92a67032b1a8357262
FenetShewarega <shewaregafenet@gmail.com>
2023-06-11 19:29:45 +0300

    updating main.dart

--- hakim_hub_mobile/lib/main.dart
+++ hakim_hub_mobile/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hakim_hub_mobile/features/core/splash_screen.dart';
 
-import 'core/shared_widgets/bottom_nav.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -11,10 +9,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) 
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashPage()
-    );
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashPage());
   }
 }
