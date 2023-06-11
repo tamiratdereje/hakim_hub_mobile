8b82b611412a834cf323c3c7421b7ff7725150f8
FenetShewarega <shewaregafenet@gmail.com>
2023-06-11 19:29:22 +0300

    colors for UI implementation

--- hakim_hub_mobile/lib/core/utils/colors.dart
+++ hakim_hub_mobile/lib/core/utils/colors.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
 
-const primaryColor = Color(0XFF68A4F4);
+const Color primaryTextColor = Colors.black;
+const Color secondryTextColor = Colors.white;
+const Color backgroundColor = Color(0xFFEFF3F3);
+const Color primaryColor = Color(0xFF68A4F4);
+const Color secondaryColor = Color(0xFFEDEDED);
+const Color chiptextColor2 = Color(0xFF6C63FF);
+const Color tertiaryTextColor = Color(0xFFFF8000);
+const Color chiptextColor1 = Color(0xFFE23636);
+const Color chiptextColor3 = Color(0xFF19B916);
