0edda49b056f4514867f1b31e70a3b83819b5ee4
Stargazing-11 <samuelkifle199314@gmail.com>
2023-06-12 16:04:27 +0300

    update on chat bot ai

--- hakim_hub_mobile/lib/core/shared_widgets/appBar.dart
+++ hakim_hub_mobile/lib/core/shared_widgets/appBar.dart
@@ -4,23 +4,21 @@ class CHSAppBar {
   static PreferredSizeWidget build(
       BuildContext context, String text, Function onPressed, bool hasPop) {
     return AppBar(
-      foregroundColor: Colors.white,
-      shadowColor: Colors.transparent,
+        foregroundColor: Colors.white,
+        shadowColor: Colors.transparent,
+        elevation: 2,
         leading: GestureDetector(
           onTap: () => {
             if (hasPop) {Navigator.pop(context)}
-          } ,
-          child: Icon(Icons.arrow_back_ios_new,
-              color: hasPop ? Colors.black : Colors.white),
+          },
+          child: const Icon(Icons.chat_bubble_outline_rounded,
+              color: Colors.black),
         ),
-          
-          
         centerTitle: true,
         title: Text(
           text,
           style: const TextStyle(color: Colors.black),
         ),
-
         backgroundColor: Colors.white);
   }
 }
--- hakim_hub_mobile/lib/features/chatbot/presentation/screen/chat_bot_landing_page.dart
+++ hakim_hub_mobile/lib/features/chatbot/presentation/screen/chat_bot_landing_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../../core/shared_widgets/appBar.dart';
+import '../../../../core/utils/icons.dart';
 
 class ChatLandingPage extends StatefulWidget {
   const ChatLandingPage({super.key});
@@ -20,26 +21,99 @@ class _ChatLandingPageState extends State<ChatLandingPage> {
         false,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Image.asset('name'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/HakimHubVectorImg.png'),
+                  Image.asset('assets/images/HakimHub.png')
+                ],
+              ),
               const Text('welcome to HakimHub'),
             ],
           ),
-          const Text(
-            'Use the power of AI to find a hospital suggestion based on your symptoms and preference',
-          ),
-          Column(
-            children: [
-              Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  color: Color.fromRGBO(237, 237, 237, 1),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 48,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Use the power of AI to find a hospital suggestion based on your symptoms and preference',
+                  style: TextStyle(),
+                  textAlign: TextAlign.center,
                 ),
-              )
-            ],
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 20,
+                  ),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    color: Color.fromRGBO(237, 237, 237, 1),
+                  ),
+                  child: const Text(
+                      'Can you recommend a hospital that specializes in [medical specialty] and has a good reputation for patient care?"'),
+                ),
+                const SizedBox(
+                  height: 19,
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 20,
+                  ),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    color: Color.fromRGBO(237, 237, 237, 1),
+                  ),
+                  child: const Text(
+                      'Can you recommend a hospital that specializes in [medical specialty] and has a good reputation for patient care?"'),
+                ),
+                const SizedBox(
+                  height: 19,
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 20,
+                  ),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    color: Color.fromRGBO(237, 237, 237, 1),
+                  ),
+                  child: const Text(
+                      'Can you recommend a hospital that specializes in [medical specialty] and has a good reputation for patient care?"'),
+                )
+              ],
+            ),
           ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 23.0),
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 17, horizontal: 31),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(26)),
+                color: Color.fromRGBO(237, 237, 237, 1),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Write a message ...'),
+                  IconButton(
+                    onPressed: () {},
+                    icon: send,
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
