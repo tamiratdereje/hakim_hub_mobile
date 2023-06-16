62276ef875690fac0cb1026159ff89540201aa58
FenetShewarega <shewaregafenet@gmail.com>
2023-06-16 14:24:31 +0300

    implement hospital detain page

--- hakim_hub_mobile/lib/features/hospital/presentation/screen/hospital_detail_page.dart
+++ hakim_hub_mobile/lib/features/hospital/presentation/screen/hospital_detail_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hakim_hub_mobile/core/utils/icons.dart';
+import 'package:hakim_hub_mobile/features/core/splash_screen.dart';
+import '../../../../core/shared_widgets/bottom_nav.dart';
+import '../widgets/hospital_card.dart';
 
 class MyPage extends StatefulWidget {
   @override
@@ -7,7 +11,6 @@ class MyPage extends StatefulWidget {
 
 class _MyPageState extends State<MyPage> {
   int _selectedIndex = -1;
-
   void _onButtonPressed(int index) {
     setState(() {
       _selectedIndex = index;
@@ -23,14 +26,56 @@ class _MyPageState extends State<MyPage> {
             top: 0,
             left: 0,
             right: 0,
-            child: Image.asset(
-              'assets/images/black_lion.png',
-              height: 200,
-              fit: BoxFit.cover,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SplashPage()),
+                );
+              },
+              child: Image.asset(
+                'assets/images/black_lion.png',
+                height: 200,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
           Positioned(
-            top: 200,
+            top: 110, // Adjust the position of the text as needed
+            left: 16,
+            child: Text(
+              'Tikur Anbessa',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          //  Positioned(
+          //   top: 110, // Adjust the position of the text as needed
+          //   left: 16,
+          //   child: IconButton(onPressed: (){}, icon: Icon.)
+          // ),
+          Positioned(
+            bottom: 10,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 40,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(top: 160, left: 30, right: 30, child: HospitalCard()),
+          Positioned(
+            top: 250,
             left: 0,
             right: 0,
             bottom: 0,
@@ -45,9 +90,9 @@ class _MyPageState extends State<MyPage> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Padding(
-                      padding: EdgeInsets.all(16),
+                      padding: EdgeInsets.all(1),
                     ),
-                    SizedBox(height: 16),
+                    SizedBox(height: 1),
                     Container(
                       height: 50,
                       decoration: BoxDecoration(
@@ -103,17 +148,33 @@ class _MyPageState extends State<MyPage> {
                       ),
                     ),
                     SizedBox(height: 16),
-                    
                     Padding(
                       padding: EdgeInsets.all(16),
                       child: Text(
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                        'Hospital Desciption',
                         style: TextStyle(
                           fontSize: 18,
                         ),
                       ),
                     ),
+                    Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
                     SizedBox(height: 16),
+                    Image.asset(
+                      '/home/fenet/Desktop/hakim_hub_mobile/hakim_hub_mobile/hakim_hub_mobile/assets/images/map.png',
+                      height: 200,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(
+                      height: 60,
+                    ),
                     Text(
                       'Services We Provide',
                       style: TextStyle(
@@ -125,61 +186,189 @@ class _MyPageState extends State<MyPage> {
                     ListView(
                       shrinkWrap: true,
                       children: [
-                        Card(
-                          child: ListTile(
-                            leading: Image.asset(
-                                "/home/fenet/Desktop/hakim_hub_mobile/hakim_hub_mobile/hakim_hub_mobile/assets/images/right_icon.png"),
-                            title: Text(
-                                'General and specialty surgical services.'),
+                        Container(
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 1,
+                                blurRadius: 5,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: Card(
+                            child: ListTile(
+                              leading: Image.asset(
+                                'assets/images/right_icon.png',
+                              ),
+                              title: Text(
+                                'General and specialty services.',
+                                style: TextStyle(fontSize: 13),
+                              ),
+                            ),
                           ),
                         ),
-                        Card(
-                          child: ListTile(
-                            leading: Image.asset(
-                                "/home/fenet/Desktop/hakim_hub_mobile/hakim_hub_mobile/hakim_hub_mobile/assets/images/right_icon.png"),
-                            title: Text('Emergency room services.'),
+                        Container(
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 1,
+                                blurRadius: 5,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: Card(
+                            child: ListTile(
+                              leading: Image.asset(
+                                'assets/images/right_icon.png',
+                              ),
+                              title: Text(
+                                'Medical consultations and checkups.',
+                                style: TextStyle(fontSize: 13),
+                              ),
+                            ),
                           ),
                         ),
-                        Card(
-                          child: ListTile(
-                            leading: Image.asset(
-                                "/home/fenet/Desktop/hakim_hub_mobile/hakim_hub_mobile/hakim_hub_mobile/assets/images/right_icon.png"),
-                            title: Text('X ray or radiology services'),
+                        Container(
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 1,
+                                blurRadius: 5,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: Card(
+                            child: ListTile(
+                              leading: Image.asset(
+                                'assets/images/right_icon.png',
+                              ),
+                              title: Text(
+                                'Diagnostic and laboratory services.',
+                                style: TextStyle(fontSize: 13),
+                              ),
+                            ),
                           ),
                         ),
-                        Card(
-                          child: ListTile(
-                            leading: Image.asset(
-                                '/home/fenet/Desktop/hakim_hub_mobile/hakim_hub_mobile/hakim_hub_mobile/assets/images/right_icon.png'),
-                            title: Text(
-                                'General and specialty surgical services.'),
+                        Container(
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromARGB(255, 201, 201, 201)
+                                    .withOpacity(0.5),
+                                spreadRadius: 1,
+                                blurRadius: 5,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
                           ),
-                        ),
-                        Card(
-                          child: ListTile(
-                            leading: Image.asset(
-                                "/home/fenet/Desktop/hakim_hub_mobile/hakim_hub_mobile/hakim_hub_mobile/assets/images/right_icon.png"),
-                            title: Text(
-                                'General and specialty surgical services.'),
+                          child: Card(
+                            child: ListTile(
+                              leading: Image.asset(
+                                'assets/images/right_icon.png',
+                              ),
+                              title: Text(
+                                'Pharmacy and medication services.',
+                                style: TextStyle(fontSize: 13),
+                              ),
+                            ),
                           ),
                         ),
                       ],
                     ),
-                    const Padding(
-                      padding: EdgeInsets.all(16),
-                      child: Text(
-                        'Available 24 hrs 5 days a week',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
-                      ),
+                    SizedBox(
+                      height: 15,
                     ),
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              clock,
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text("Available 24 hrs  5 days a week"),
+                              )
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              location,
+                              SizedBox(
+                                width: 30,
+                                child: Text(
+                                  "Bole, In front of Rwanda Embassy, Addis Ababa, Ethiopia",
+                                  maxLines: 2,
+                                ),
+                              ),
+                            ],
+                          ),
+                          TextButton(
+                              onPressed: () {}, child: Text("SEE IT ON MAP"))
+                        ])
                   ],
                 ),
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ServiceCard extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  const ServiceCard({
+    Key? key,
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      margin: EdgeInsets.only(right: 16),
+      // decoration: BoxDecoration(
+      //   color: Colors.white,
+      //   borderRadius: BorderRadius.circular(17),
+      // ),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(
+              icon,
+              size: 60,
+              color: Theme.of(context).primaryColor,
+            ),
+            SizedBox(height: 16),
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              subtitle,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
