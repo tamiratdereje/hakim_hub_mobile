0268ecee7a51f1c89375308fdd95383182a73b5c
tamiratdereje <tamiratdereje167@gmail.com>
2023-06-20 12:08:12 +0300

    added models for hospitals

---
+++ hakim_hub_mobile/lib/features/hospital/data/models/institutional_doctors.dart
@@ -0,0 +1,41 @@
+
+
+
+
+class Doctor {
+  String fullName;
+  String about;
+  String gender;
+  String? email;
+  String photoUrl;
+  int yearsOfExperience;
+  String mainInstitutionId;
+  List<String> specialities;
+  String id;
+
+  Doctor({
+    required this.fullName,
+    required this.about,
+    required this.gender,
+    required this.email,
+    required this.photoUrl,
+    required this.yearsOfExperience,
+    required this.mainInstitutionId,
+    required this.specialities,
+    required this.id,
+  });
+
+  factory Doctor.fromJson(Map<String, dynamic> json) {
+    return Doctor(
+      fullName: json['fullName'],
+      about: json['about'],
+      gender: json['gender'],
+      email: json['email'],
+      photoUrl: json['photoUrl'],
+      yearsOfExperience: json['yearsOfExperience'],
+      mainInstitutionId: json['mainInstitutionId'],
+      specialities: List<String>.from(json['specialities']),
+      id: json['id'],
+    );
+  }
+}
