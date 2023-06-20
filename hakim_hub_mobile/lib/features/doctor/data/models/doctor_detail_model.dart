f032981f06ae3173b04dea0f569138ee0eb5fac0
tamiratdereje <tamiratdereje167@gmail.com>
2023-06-20 14:31:34 +0300

    add doctor detail page

---
+++ hakim_hub_mobile/lib/features/doctor/data/models/doctor_detail_model.dart
@@ -0,0 +1,51 @@
+
+
+import 'doctor_education_model.dart';
+import 'doctor_experiance.dart';
+
+class DoctorDetail {
+  String fullName;
+  String about;
+  String gender;
+  String email;
+  String photoUrl;
+  int yearsOfExperience;
+  String mainInstitutionId;
+  String mainInstitutionName;
+  List<String> specialities;
+  List<Education> educations;
+  List<Experience> experiences;
+  String id;
+
+  DoctorDetail({
+    required this.fullName,
+    required this.about,
+    required this.gender,
+    required this.email,
+    required this.photoUrl,
+    required this.yearsOfExperience,
+    required this.mainInstitutionId,
+    required this.mainInstitutionName,
+    required this.specialities,
+    required this.educations,
+    required this.experiences,
+    required this.id,
+  });
+
+  factory DoctorDetail.fromJson(Map<String, dynamic> json) {
+    return DoctorDetail(
+      fullName: json['fullName'],
+      about: json['about'],
+      gender: json['gender'],
+      email: json['email'],
+      photoUrl: json['photoUrl'],
+      yearsOfExperience: json['yearsOfExperience'],
+      mainInstitutionId: json['mainInstitutionId'],
+      mainInstitutionName: json['mainInstitutionName'],
+      specialities: List<String>.from(json['specialities']),
+      educations: (json['educations'] as List<dynamic>).map((e) => Education.fromJson(e)).toList(),
+      experiences: (json['experiences'] as List<dynamic>).map((e) => Experience.fromJson(e)).toList(),
+      id: json['id'],
+    );
+  }
+}
