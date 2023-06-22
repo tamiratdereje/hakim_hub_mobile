81c8fe0ca505b769edec448b4b75bc148eaecc3e
Stargazing-11 <samuelkifle199314@gmail.com>
2023-06-22 10:25:01 +0300

    changes made

--- hakim_hub_mobile/lib/features/doctor/data/datasources/doctor_detail_remote_datasource.dart
+++ hakim_hub_mobile/lib/features/doctor/data/datasources/doctor_detail_remote_datasource.dart
@@ -21,7 +21,9 @@ class DoctorDetailRemoteDataSourceImpl implements DoctorDetailRemoteDataSource {
   @override
   Future<DoctorDetailModel> getDoctorDetail(String id) async {
     final response = await client.get(
-      Uri.parse("${baseUrl}/api/DoctorProfiles/${id}"),
+      // Uri.parse("${baseUrl}/api/DoctorProfiles/${id}"),
+      Uri.parse(
+          'https://hakim-hub.onrender.com/api/DoctorProfiles/02fb54e9-ebbf-48a5-acdf-12c0d51ec264'),
       headers: {
         'Content-Type': 'application/json',
       },
