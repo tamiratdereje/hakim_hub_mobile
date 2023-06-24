dde93a45c1ab0cc223b06fdbbce81f84212eb19c
bereket <bereketdemissie4382@gmail.com>
2023-06-24 07:27:59 +0300

    feature(hakimhub-mobile) implement chat model

---
+++ hakim_hub_mobile/lib/features/chatbot/data/models/chat_model.dart
@@ -0,0 +1,14 @@
+import 'package:hakim_hub_mobile/features/chatbot/domain/entities/chat_request_entity.dart';
+
+// ignore: must_be_immutable
+class ChatRequestModel extends ChatRequest {
+  ChatRequestModel({
+    required String address,
+    required bool isNew,
+    required String message,
+  }) : super(
+    address : address,
+    isNew : isNew,
+    message : message,
+  );
+}
