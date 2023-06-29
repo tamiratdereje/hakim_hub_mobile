728b89346e25187aa0dce1d28506e60f2e600a73
EyasuTesfu <jt272525@gmail.com>
2023-06-29 14:49:55 +0300

    add some features

--- hakim_hub_mobile/lib/features/hospital/presentation/widgets/doctor_grid_view.dart
+++ hakim_hub_mobile/lib/features/hospital/presentation/widgets/doctor_grid_view.dart
@@ -4,7 +4,6 @@ import 'package:hakim_hub_mobile/core/utils/icons.dart';
 import 'package:hakim_hub_mobile/core/utils/ui_converter.dart';
 import 'package:hakim_hub_mobile/features/hospital/domain/entities/hospital_doctor_domain.dart';
 import 'package:hakim_hub_mobile/features/hospital/presentation/widgets/doctor_card.dart';
-import 'package:hakim_hub_mobile/features/hospital/presentation/widgets/doctors_list.dart';
 
 import '../../../../core/utils/colors.dart';
 import '../../../../router/routes.dart';
@@ -52,7 +51,7 @@ class _DoctorGridViewState extends State<DoctorGridView> {
     }
     return doctors
         .where((doctor) =>
-            doctor.fullName.toLowerCase().contains(searchWord.toLowerCase()) &&
+            doctor.fullName.toLowerCase().contains(searchWord.toLowerCase()) ||
             doctor.specialities.any((speciality) =>
                 speciality.toLowerCase().contains(searchWord.toLowerCase())))
         .toList();
@@ -73,89 +72,93 @@ class _DoctorGridViewState extends State<DoctorGridView> {
 
   @override
   Widget build(BuildContext context) {
-    return FocusScope(
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: UIConverter.getComponentWidth(context, 10),
-          vertical: UIConverter.getComponentHeight(context, 20),
-        ),
-        child: Column(
-          children: [
-            FormField(
-              key: formkey,
-              builder: (FormFieldState state) {
-                return SizedBox(
-                  width: UIConverter.getComponentWidth(context, 370),
-                  height: UIConverter.getComponentHeight(context, 48),
-                  child: TextFormField(
-                    onChanged: _onSearch,
-                    focusNode: _searchFocusNode,
-                    onFieldSubmitted: _onSearchSubmitted,
-                    decoration: InputDecoration(
-                      hintText: "Search Doctor or Speciality",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      prefixIcon: search,
-                      suffixIcon: GestureDetector(
-                        child: filter,
-                        onTap: () async {
-                          DoctorFilterDomain doctorFilterDomain =
-                              (await showModalBottomSheet<DoctorFilterDomain>(
-                            isScrollControlled: true,
-                            backgroundColor: titleTextColor,
-                            context: context,
-                            builder: (BuildContext context) {
-                              return DoctorFilterPage(
-                                filterList: widget.filterList,
-                                institutionId: widget.institutionId,
-                              );
-                            },
-                          ))!;
-                          print("anything");
-                          widget.onFilterChanged(doctorFilterDomain);
-                        },
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: UIConverter.getComponentWidth(context, 10),
+        vertical: UIConverter.getComponentHeight(context, 20),
+      ),
+      child: GestureDetector(
+        onTap: () => _searchFocusNode.unfocus(),
+        behavior: HitTestBehavior.translucent,
+        child: FocusScope(
+          child: Column(
+            children: [
+              FormField(
+                key: formkey,
+                builder: (FormFieldState state) {
+                  return SizedBox(
+                    width: UIConverter.getComponentWidth(context, 370),
+                    height: UIConverter.getComponentHeight(context, 48),
+                    child: TextFormField(
+                      focusNode: _searchFocusNode,
+                      onChanged: _onSearch,
+                      onFieldSubmitted: _onSearchSubmitted,
+                      decoration: InputDecoration(
+                        hintText: "Search Doctor or Speciality",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        prefixIcon: search,
+                        suffixIcon: GestureDetector(
+                          child: filter,
+                          onTap: () async {
+                            DoctorFilterDomain doctorFilterDomain =
+                                (await showModalBottomSheet<DoctorFilterDomain>(
+                              isScrollControlled: true,
+                              backgroundColor: titleTextColor,
+                              context: context,
+                              builder: (BuildContext context) {
+                                return DoctorFilterPage(
+                                  filterList: widget.filterList,
+                                  institutionId: widget.institutionId,
+                                );
+                              },
+                            ))!;
+                            print("anything");
+                            widget.onFilterChanged(doctorFilterDomain);
+                          },
+                        ),
                       ),
                     ),
-                  ),
-                );
-              },
-            ),
-            SizedBox(
-              height: UIConverter.getComponentHeight(context, 20),
-            ),
-            Expanded(
-              child: GridView.builder(
-                itemCount: _filteredDoctors.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, // Change this to 4 for a 4x4 grid
-                  crossAxisSpacing:
-                      UIConverter.getComponentWidth(context, 17.78),
-                  mainAxisSpacing:
-                      UIConverter.getComponentHeight(context, 28.31),
-                  childAspectRatio:
-                      UIConverter.getComponentWidth(context, 179.22) /
-                          UIConverter.getComponentHeight(context, 139.69),
-                ),
-                itemBuilder: (BuildContext context, int index) {
-                  final doctor = _filteredDoctors[index];
-                  return GestureDetector(
-                    child: DoctorCard(
-                      imageUrl: doctor.photoUrl,
-                      title: doctor.fullName,
-                      subtitle: doctor.specialities.join(", "),
-                    ),
-                    onTap: () {
-                      context.pushNamed(
-                        AppRoutes.DoctorDetailPage,
-                        queryParameters: {"id": doctor.id},
-                      );
-                    },
                   );
                 },
               ),
-            ),
-          ],
+              SizedBox(
+                height: UIConverter.getComponentHeight(context, 20),
+              ),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: _filteredDoctors.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // Change this to 4 for a 4x4 grid
+                    crossAxisSpacing:
+                        UIConverter.getComponentWidth(context, 17.78),
+                    mainAxisSpacing:
+                        UIConverter.getComponentHeight(context, 28.31),
+                    childAspectRatio:
+                        UIConverter.getComponentWidth(context, 179.22) /
+                            UIConverter.getComponentHeight(context, 139.69),
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    final doctor = _filteredDoctors[index];
+                    return GestureDetector(
+                      child: DoctorCard(
+                        imageUrl: doctor.photoUrl,
+                        title: doctor.fullName,
+                        subtitle: doctor.specialities.join(", "),
+                      ),
+                      onTap: () {
+                        context.pushNamed(
+                          AppRoutes.DoctorDetailPage,
+                          queryParameters: {"id": doctor.id},
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
