5559b86fb7df2bcd72f2f6ce3c40ebe26fd14432
Stargazing-11 <samuelkifle199314@gmail.com>
2023-07-30 15:36:41 +0300

    added equatable to entities which didn't have equatable

--- hakim_hub_mobile/lib/features/doctor/domain/entities/doctor_experience.dart
+++ hakim_hub_mobile/lib/features/doctor/domain/entities/doctor_experience.dart
@@ -1,15 +1,20 @@
+import 'package:equatable/equatable.dart';
 
-class Experience {
+class Experience extends Equatable {
   String title;
   String description;
   String institution;
   DateTime startDate;
   DateTime endDate;
 
-  Experience(
-      {required this.title,
-      required this.institution,
-      required this.startDate,
-      required this.endDate,
-      required this.description});
+  Experience({
+    required this.title,
+    required this.institution,
+    required this.startDate,
+    required this.endDate,
+    required this.description,
+  }) : super();
+
+  @override
+  List<Object> get props => [];
 }
