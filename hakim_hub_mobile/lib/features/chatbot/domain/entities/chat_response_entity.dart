f92e33a97ee3cc68741d7f09600dbf6157f72bc7
bereket <bereketdemissie4382@gmail.com>
2023-08-15 10:56:05 +0300

    write documentation for chat feature domain layer

--- hakim_hub_mobile/lib/features/chatbot/domain/entities/chat_response_entity.dart
+++ hakim_hub_mobile/lib/features/chatbot/domain/entities/chat_response_entity.dart
@@ -1,14 +1,24 @@
 import 'package:equatable/equatable.dart';
 import 'package:hakim_hub_mobile/features/chatbot/domain/entities/chat_institute_entity.dart';
 
+/// Represents a chatbot's response to a user's request.
+/// This class encapsulates a response from the chatbot, including the [reply] text,
+/// a list of associated [institutes] that might be relevant, and a [speciality]
+/// indicating a specific area of expertise.
 class ChatResponse extends Equatable {
-  String reply;
-  List<ChatInstituteEntity> institutes;
-  String speciality;
+  /// The chatbot's reply text.
+  final String reply;
 
-  ChatResponse({
-    required this.institutes,
+  /// A list of institutes related to the chatbot's response.
+  final List<ChatInstituteEntity> institutes;
+
+  /// A specific area of expertise indicated in the chatbot's response.
+  final String speciality;
+
+  /// Creates a [ChatResponse] instance with the given details.
+  const ChatResponse({
     required this.reply,
+    required this.institutes,
     required this.speciality,
   });
 
--- hakim_hub_mobile/lib/features/chatbot/domain/usecases/get_chat_response.dart
+++ hakim_hub_mobile/lib/features/chatbot/domain/usecases/get_chat_response.dart
@@ -5,11 +5,21 @@ import 'package:hakim_hub_mobile/features/chatbot/domain/entities/chat_request_e
 import 'package:hakim_hub_mobile/features/chatbot/domain/entities/chat_response_entity.dart';
 import 'package:hakim_hub_mobile/features/chatbot/domain/repositories/chat_repository.dart';
 
+/// Fetches chatbot responses based on user requests.
+/// The responses are obtained in response to the given [ChatRequest].
 class GetChatResponse extends UseCase<ChatResponse, ChatRequest> {
   final ChatRepository repository;
 
+  /// Constructs a [GetChatResponse] instance with the required [repository].
+  /// The [repository] is an instance of the `ChatRepository` used to fetch chatbot responses.
   GetChatResponse({required this.repository});
 
+  /// Fetches a chatbot response for the provided [chatRequest].
+
+/// This function can produce a successful [ChatResponse] containing the requested
+/// information or, in case of an error, a [Failure].
+
+  
   Future<Either<Failure, ChatResponse>> call(ChatRequest chatRequest) async {
     return await repository.getChatResponse(chatRequest);
   }
